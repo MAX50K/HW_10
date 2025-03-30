4d4ff46cdce111d8b861f69a1267d01704e03acd
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-03-30 14:32:23 +0200

    jj

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@
 // const getSum = arr => arr.reduce((acc, curr) => acc + curr, 0);
 // const getMin = arr => Math.min(...arr);
 // const getMax = arr => Math.max(...arr);
-const processArray = (array, callback) => callback(array)
+const numbers = [3, 7, 1, 9, 4];
+const processArray = (array, callback) => {callback(array)}
 ;
 const getSum = function(arr) {return arr.reduce((acc, curr) => acc + curr, 0)}
 const getMin = function(arr) {return Math.min(...arr)}
 const getMax = function(arr) {return Math.max(...arr)}
 
-const numbers = [3, 7, 1, 9, 4];
 console.log("Сума:", processArray(numbers, getSum));
 console.log("Мінімальне значення:", processArray(numbers, getMin));
 console.log("Максимальне значення:", processArray(numbers, getMax))
@@ -20,8 +20,7 @@ const operate = (a, b, callback) => {
     callback(a, b);
 };
 
-// Виклик функції з різними операціями
-console.log(operate(10, 5, function(a, b){ return a - b})); // Віднімання
-console.log(operate(10, 5, function(a, b){ return a + b})); // Додавання
-console.log(operate(10, 5, function(a, b){ return a * b})); // Множення
-console.log(operate(10, 5, function(a, b){ return a / b})); // Ділення
+console.log(operate(10, 5, function(a, b){ return a - b}));
+console.log(operate(10, 5, function(a, b){ return a + b}));
+console.log(operate(10, 5, function(a, b){ return a * b}));
+console.log(operate(10, 5, function(a, b){ return a / b}));
